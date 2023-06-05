7054402c7a8a23163cc982f0200bd7eab015f441
Edem Essang <edemoski.eu@gmail.com>
2023-06-05 07:46:10 +0100

    0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,27 @@
 #!/usr/bin/node
+
 const request = require('request');
 
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+const req = (arr, i) => {
+  if (i === arr.length) return;
+  request(arr[i], (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log(JSON.parse(body).name);
+      req(arr, i + 1);
+    }
+  });
+};
 
-request(url, (err, res, body) => {
-  if (err) {
-    console.log(err);
+request(
+  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
+  (err, response, body) => {
+    if (err) {
+      throw err;
+    } else {
+      const chars = JSON.parse(body).characters;
+      req(chars, 0);
+    }
   }
-  const characters = JSON.parse(body).characters;
-  const promises = characters.map((character) => {
-    return new Promise((resolve, reject) => {
-      request(character, (err, res, body) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(JSON.parse(body).name);
-      });
-    });
-  }
-  );
-  Promise.all(promises).then((names) => {
-    names.forEach((name) => {
-      console.log(name);
-    });
-  }
-  );
-});
+);
